0c9dd3b500feb1f0e0f688b44f7b16e425c6628a
Dragos Tudorache <hedphug@gmail.com>
2016-03-29 07:03:46 +0300

    add second react callback inisde the send_message capability; todo: doesn't work as expected :D

--- lib/kapyz/capability/send_message.ex
+++ lib/kapyz/capability/send_message.ex
@@ -2,6 +2,11 @@ defmodule Dobar.Kapyz.Capability.SendMessage do
   use Dobar.Kapyz.Capability, name: :send_message
   alias Dobar.Models.Intent
 
+  # TODO: find out why this doesn't work
+  def react(%Intent{entities: %{email: email}}) do
+    IO.puts "pfff,a re si email???"
+  end
+
   def react(%Intent{input: text}) do
     IO.puts "should react to the :send_text_message.ex intention"
     IO.puts "text data: #{inspect text}"
