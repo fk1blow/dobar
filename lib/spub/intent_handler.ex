5654932ab21449c550899152efbe71e9c9e531b6
Dragos Tudorache <hedphug@gmail.com>
2016-03-29 06:45:59 +0300

    add the spub/intent handler module

---
+++ lib/spub/intent_handler.ex
@@ -0,0 +1,35 @@
+defmodule Dobar.Spub.IntentHandler do
+  @moduledoc """
+    Has the responsability to handle events and notifications related to an intent.
+    An intent is an expression that was evaluated and its intention was detected.
+
+    TODO: should be renamed to intent parser handler and break the parsing
+    from the processing(coming as a result of the capability) stages in two
+    distinct event handlers
+      - when the response from wit.ai has arrived
+      - when the capability has resolved the intention
+  """
+
+  require Logger
+
+  use GenEvent
+
+  def handle_event({:intention_evaluated, {:error, error}}, state) do
+    # TODO: what should i do with this?!?!
+    Logger.info "error while trying to evaluate the input intent"
+    {:ok, state}
+  end
+
+  # triggered from `Intent.Evaluator`
+  def handle_event({:intention_evaluated, intent}, state) do
+    Dobar.Intent.Resolver.evaluate_intent intent
+    Logger.info "intention evaluated to: #{inspect intent}"
+    {:ok, state}
+  end
+
+  def handle_event({:capability_evaluated, capability}, state) do
+    Logger.debug "should distribute the results of the capability"
+    {:ok, state}
+  end
+end
+
