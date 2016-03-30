c4d54a12b57efc1eba6d6df18fa9cb70b7bf65de
Dragos Tudorache <hedphug@gmail.com>
2016-03-31 01:49:00 +0300

    fix text input type reference

--- lib/interface/receiver/text_receiver.ex
+++ lib/interface/receiver/text_receiver.ex
@@ -1,12 +1,12 @@
 defmodule Dobar.Interface.Receiver.Text do
   @behaviour Dobar.Interface.Receiver
 
-  alias Dobar.Model.Input
+  alias Dobar.Model.Input.Text
 
   def parse(nil) do
     raise "cannot evaluate nil as an input!"
   end
   def parse(input) do
-    Dobar.Intent.Resolver.evaluate_input %Input{type: :text, data: input}
+    Dobar.Intent.Resolver.evaluate_input %Text{type: :text, data: input}
   end
 end
